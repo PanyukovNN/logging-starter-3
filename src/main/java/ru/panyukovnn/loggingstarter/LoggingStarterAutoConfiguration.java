3e68ded260669b9a27ab56904c8247b523339322
Nikolay Panyukov <panyukovnn@gmail.com>
2025-02-15 11:17:39 +0300

    initial commit

---
+++ src/main/java/ru/panyukovnn/loggingstarter/LoggingStarterAutoConfiguration.java
@@ -0,0 +1,8 @@
+package ru.panyukovnn.loggingstarter;
+
+public class LoggingStarterAutoConfiguration {
+
+    public static void println(String input) {
+        System.out.println("Выведено из стартера: " + input);
+    }
+}
