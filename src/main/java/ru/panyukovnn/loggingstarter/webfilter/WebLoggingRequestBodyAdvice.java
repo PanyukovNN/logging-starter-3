6c6a046bb1e3439671e2948e7c253e8761286e26
Nikolay Panyukov <panyukovnn@gmail.com>
2025-03-09 11:11:24 +0300

    HW-10: Добавлен ControllerAdvice; Публикация версии 1.2

--- src/main/java/ru/panyukovnn/loggingstarter/webfilter/WebLoggingRequestBodyAdvice.java
+++ src/main/java/ru/panyukovnn/loggingstarter/webfilter/WebLoggingRequestBodyAdvice.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.HttpInputMessage;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdviceAdapter;
 import ru.panyukovnn.loggingstarter.service.LoggingService;
 
 import java.lang.reflect.Type;
 
+@ControllerAdvice
 public class WebLoggingRequestBodyAdvice extends RequestBodyAdviceAdapter {
 
     @Autowired
